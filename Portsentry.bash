77ffe4f4008c78433e21e8649e67374a05aadcb3
dbrya7 <44570531+dbrya7@users.noreply.github.com>
2018-11-13 21:00:39 +0800

    Add files via upload

---
+++ Portsentry.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install portsentry
+sed -i 's/RESOLVE_HOST = "0"/RESOLVE_HOST = "1"/' /etc/portsentry/portsentry.conf
+sed -i 's/BLOCK_UDP="0"/BLOCK_UDP="1"/' /etc/portsentry/portsentry.conf
+sed -i 's/SCAN_TRIGGER="0"/SCAN_TRIGGER="1"/' /etc/portsentry/portsentry.conf
+
